73ba4accb2e95fa85d91dfe833c18d32eaa06d72
Erik Haraldson <erikharaldson@MacBook-Pro.lan>
2022-12-28 10:04:02 +0100

    Adds a function to perform cross validation on a model

--- modelling.py
+++ modelling.py
@@ -1,13 +1,53 @@
 from tabular_data import load_airbnb
-from sklearn import linear_model, model_selection, metrics, preprocessing
+from sklearn import linear_model, model_selection, metrics, preprocessing, datasets
+from itertools import product
 
 import numpy as np
 np.random.seed(20)
 
 # Completes cross validation of a model on different parameters and returns the best model
 def custom_tune_regression_model_hyperparameters(model_class, X_train, y_train, X_val, y_val, X_test, y_test, parameter_dictionary):
+    '''
+    Example usage:
+    p_dict = {'alpha': [10,1,0.1,0.01],
+            'max_iter': [100, 200, 300, 400]}
 
-    pass
+    data = {"X_train": X_train, "y_train": y_train, "X_val": X_val, "y_val": y_val, "X_test": X_test, "y_test": y_test}
+    model, hyperparamters, performance_metrics = custom_tune_regression_model_hyperparameters(model_class=linear_model.Ridge, **data, parameter_dictionary=p_dict)
+    '''
+    hyperparameter_combinations = [dict(zip(parameter_dictionary, v)) for v in product(*parameter_dictionary.values())]
+
+    best_rmse = np.inf
+
+    for hyperparameter_combination in hyperparameter_combinations:
+
+        model = model_class(**hyperparameter_combination)
+        model.fit(X_train, y_train)
+
+        y_hat = model.predict(X_val)
+        rmse = metrics.mean_squared_error(y_val, y_hat, squared=False)
+
+        print(model, rmse)
+
+        if rmse < best_rmse:
+
+            best_model = model
+            best_rmse = rmse
+            best_hyperparamater_values = hyperparameter_combination
+            
+            validation_r2_score = model.score(X_val, y_val)
+
+            y_hat = model.predict(X_test)
+            test_rmse = metrics.mean_squared_error(y_test, y_hat, squared=False)
+            test_r2_score = model.score(X_test, y_test)
+
+            best_performance_metrics = {'validation_RMSE': rmse,
+                                        'validation_R2': validation_r2_score,
+                                        'test_RMSE': test_rmse,
+                                        'test_R2': test_r2_score}
+
+    return best_model, best_hyperparamater_values, best_performance_metrics
+
 
 # Load data
 X, y = load_airbnb()

2155d9cdec074e80f81fdff043833cd1b00c0e3a
Erik Haraldson <erikharaldson@MacBook-Pro.lan>
2022-12-28 14:04:17 +0100

    Saves the best linear regression model in a folder along with metrics and hyperparameters

--- modelling.py
+++ modelling.py
@@ -2,6 +2,9 @@ from tabular_data import load_airbnb
 from sklearn import linear_model, model_selection, metrics, preprocessing, pipeline
 from itertools import product
 
+import os
+import json
+import pickle
 import numpy as np
 np.random.seed(20)
 
@@ -50,17 +53,12 @@ def custom_tune_regression_model_hyperparameters(model_class, X_train, y_train, 
 
 # Uses sklearn's GridSearchCV on a linear regression model to find the best model
 def tune_regression_model_hyperparameters(X_train, y_train, X_test, y_test):
-    # hyperparameters = {'loss': ['squared_error', 'huber', 'epsilon_insensitive', 'squared_epsilon_insensitive'],
-    #                    'penalty': ['l1', 'l2', 'elasticnet', None],
-    #                    'alpha': [10, 1, 0.1, 0.001, 0.0001, 0.00001],
-    #                    'l1_ratio': [0.05, 0.1, 0.15, 0.2, 0.25, 0.3],
-    #                    'epsilon': [0.05, 0.1, 0.15, 0.2]
-    # }
 
     hyperparameters = {'penalty': ['l1', 'l2'],
-                       'alpha': [10, 1, 0.1, 0.001, 0.0001, 0.00001, 0]}
+                       'alpha': [10, 1, 0.1, 0.001, 0.0001, 0.00001, 0],
+                       'max_iter': [1000, 10000, 100000, 1000000]}
 
-    clf = model_selection.GridSearchCV(estimator=linear_model.SGDRegressor(max_iter=100000),
+    clf = model_selection.GridSearchCV(estimator=linear_model.SGDRegressor(),
                                        scoring='neg_root_mean_squared_error',
                                        param_grid=hyperparameters, 
                                        verbose=3,
@@ -68,18 +66,37 @@ def tune_regression_model_hyperparameters(X_train, y_train, X_test, y_test):
 
     clf.fit(X_train, y_train)
 
-    print(clf.best_params_)
-    print(clf.best_score_)
-    print()
+    best_model = clf.best_estimator_
+
+    model_hyperparamters = clf.best_params_
+    validation_rmse_score = -clf.best_score_
 
     y_hat = clf.predict(X_test)
-    test_rmse = metrics.mean_squared_error(y_test, y_hat)
-    test_score = clf.score(X_test, y_test)
+    test_rmse_score = -clf.score(X_test, y_test)
+    test_r2_score = metrics.r2_score(y_test, y_hat)
 
-    print(test_rmse)
-    print(test_score)
+    best_score_metrics = {'validation_RMSE': validation_rmse_score, 'test_RMSE': test_rmse_score, 'test_R2': test_r2_score}
 
-    return clf
+    return best_model, model_hyperparamters, best_score_metrics
+
+# Function to save model
+def save_model(folder, model, model_hyperparameters, model_score_metrics):
+
+    cwd = os.getcwd()
+    model_filename = os.path.join(cwd, folder+'/model.joblib')
+    hyperparameter_filename = os.path.join(cwd, folder+'/hyperparameters.json')
+    score_filename = os.path.join(cwd, folder+'/metrics.json')
+
+    # Save model
+    pickle.dump(model, open(model_filename, 'wb'))
+
+    # Save hyperparameters
+    with open(hyperparameter_filename, 'w') as outfile:
+        json.dump(model_hyperparameters, outfile)
+
+    # Save metrics
+    with open(score_filename, 'w') as outfile:
+        json.dump(model_score_metrics, outfile)
 
 ## Model 2: SGDRegressor =>
 X, y = load_airbnb()
@@ -94,46 +111,26 @@ scaler = preprocessing.StandardScaler().fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-model = tune_regression_model_hyperparameters(X_train, y_train, X_test, y_test)
+model, model_hyperparameters, model_score_metrics = tune_regression_model_hyperparameters(X_train, y_train, X_test, y_test)
+print(model)
 
-# X, y = load_airbnb()
-# X = np.array(X)
-# y = np.array(y).reshape(-1)
+save_model('models/regression/linear_regression', model, model_hyperparameters, model_score_metrics)
 
-# # Split data into train, validation and test data
-# X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-# X_train, X_val, y_train, y_val = model_selection.train_test_split(X_train, y_train, test_size=0.2)
 
-# scaler = preprocessing.StandardScaler().fit(X_train)
-# X_train = scaler.transform(X_train)
-# X_val = scaler.transform(X_val)
-# X_test = scaler.transform(X_test)
 
-# model = linear_model.SGDRegressor(penalty=None)
-# model.fit(X_train, y_train)
 
-# # Print model coefficients
-# print(model.coef_)
-# print(model.intercept_)
-# print() 
 
-# # Print R2 score on validation dataset
-# print(f'R^2 score: {model.score(X_val, y_val)}')
-# print()
 
-# # Print RMSE score on validation dataset
-# y_hat = model.predict(X_val)
-# print(f'RMSE score: {metrics.mean_squared_error(y_val, y_hat, squared=False)}')
-# print()
 
-# # Print R2 score on test dataset
-# print(f'R^2 score on test: {model.score(X_test, y_test)}')
-# print()
 
-# # Print RMSE score on test dataset
-# y_hat = model.predict(X_test)
-# print(f'RMSE score on test: {metrics.mean_squared_error(y_test, y_hat, squared=False)}')
-# print()
+
+
+
+
+
+
+
+
 
 ## Model 1: LinearRegression =>
 ''' 

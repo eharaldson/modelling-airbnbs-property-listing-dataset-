f005bc9bf6bc474f4a1adae3885d628bf52677b0
Erik Haraldson <erikharaldson@MacBook-Pro.local>
2023-01-06 15:06:23 +0000

    Adds function that returns classification metrics and a hyperparameter tuning function for classification

--- modelling.py
+++ modelling.py
@@ -1,5 +1,5 @@
 from tabular_data import load_airbnb
-from sklearn import linear_model, model_selection, metrics, preprocessing, tree, ensemble, svm, neighbors
+from sklearn import linear_model, model_selection, metrics, preprocessing, tree, ensemble, svm, neighbors, datasets
 from itertools import product
 
 import os
@@ -54,7 +54,7 @@ def custom_tune_regression_model_hyperparameters(model_class, X_train, y_train, 
     return best_model, best_hyperparamater_values, best_performance_metrics
 
 # Uses sklearn's GridSearchCV on a linear regression model to find the best model
-def tune_regression_model_hyperparameters(estimator, X_train, y_train, X_test, y_test, hyperparameters):
+def tune_regression_model_hyperparameters(estimator, X_train, X_test, y_train, y_test, hyperparameters):
 
     clf = model_selection.GridSearchCV(estimator=estimator,
                                        scoring='neg_root_mean_squared_error',
@@ -99,7 +99,7 @@ def save_model(folder, model, model_hyperparameters, model_score_metrics):
         json.dump(model_score_metrics, outfile)
 
 # Function for running GridSearchCV for Adaboost
-def adaboost_CV(X_train, y_train, X_test, y_test, save_ = True):
+def adaboost_CV(X_train, X_test, y_train, y_test, save_ = True):
     hyperparameters = {'estimator': [tree.DecisionTreeRegressor(), svm.NuSVR(), linear_model.SGDRegressor(), neighbors.KNeighborsRegressor()],
                    'n_estimators': [5, 20, 40, 80, 150, 250]}
     estimator = ensemble.AdaBoostRegressor()
@@ -109,7 +109,7 @@ def adaboost_CV(X_train, y_train, X_test, y_test, save_ = True):
         save_model('models/regression/adaboost', model, model_hyperparameters, model_score_metrics)
 
 # Function for running GridSearchCV for gradient boosting
-def gradientboost_CV(X_train, y_train, X_test, y_test, save_ = True):
+def gradientboost_CV(X_train, X_test, y_train, y_test, save_ = True):
     hyperparameters = {'n_estimators': [5, 20, 40, 80, 150, 250],
                 'loss': ['squared_error', 'absolute_error', 'huber', 'quantile'],
                 'learning_rate': [0.5, 1, 5, 10]}
@@ -120,7 +120,7 @@ def gradientboost_CV(X_train, y_train, X_test, y_test, save_ = True):
         save_model('models/regression/gradient_boosting', model, model_hyperparameters, model_score_metrics)
 
 # Function for running GridSearchCV for random forest
-def random_forest_CV(X_train, y_train, X_test, y_test, save_ = True):
+def random_forest_CV(X_train, X_test, y_train, y_test, save_ = True):
     hyperparameters = {'criterion': ["squared_error", "absolute_error", "friedman_mse", "poisson"],
                     'n_estimators': [5, 20, 50, 100, 200],
                     'max_depth': [1, 5, 25, 50, 100, 200],
@@ -133,7 +133,7 @@ def random_forest_CV(X_train, y_train, X_test, y_test, save_ = True):
         save_model('models/regression/random_forest', model, model_hyperparameters, model_score_metrics)
 
 # Function for running GridSearchCV for decision tree regressor
-def decision_tree_CV(X_train, y_train, X_test, y_test, save_ = True):
+def decision_tree_CV(X_train, X_test, y_train, y_test, save_ = True):
     hyperparameters = {'criterion': ["squared_error", "friedman_mse", "absolute_error", "poisson"],
                     'max_depth': [1, 5, 25, 50, 100, 200]}
     estimator = tree.DecisionTreeRegressor()
@@ -143,7 +143,7 @@ def decision_tree_CV(X_train, y_train, X_test, y_test, save_ = True):
         save_model('models/regression/decision_tree', model, model_hyperparameters, model_score_metrics)
 
 # Function for running GridSearchCV for sgd regressor
-def sgd_regressor_CV(X_train, y_train, X_test, y_test, save_ = True):
+def sgd_regressor_CV(X_train, X_test, y_train, y_test, save_ = True):
     hyperparameters = {'penalty': ['l1', 'l2'],
                         'alpha': [10, 1, 0.1, 0.001, 0.0001, 0.00001, 0],
                         'max_iter': [1000, 10000, 100000, 1000000]}
@@ -154,17 +154,17 @@ def sgd_regressor_CV(X_train, y_train, X_test, y_test, save_ = True):
         save_model('models/regression/linear_regression', model, model_hyperparameters, model_score_metrics)    
 
 # Function for running GridSearchCV on all the regression models
-def evaluate_all_models(X_train, y_train, X_test, y_test, save_ = True):
+def evaluate_all_regression_models(X_train, X_test, y_train, y_test, save_ = True):
 
     # Run the regression models and save the best one after cross validation
-    sgd_regressor_CV(X_train, y_train, X_test, y_test)
-    decision_tree_CV(X_train, y_train, X_test, y_test)
-    random_forest_CV(X_train, y_train, X_test, y_test)
-    gradientboost_CV(X_train, y_train, X_test, y_test)
-    adaboost_CV(X_train, y_train, X_test, y_test)
+    sgd_regressor_CV(X_train, X_test, y_train, y_test)
+    decision_tree_CV(X_train, X_test, y_train, y_test)
+    random_forest_CV(X_train, X_test, y_train, y_test)
+    gradientboost_CV(X_train, X_test, y_train, y_test)
+    adaboost_CV(X_train, X_test, y_train, y_test)
 
 # Find the best model and return it
-def find_best_model():
+def find_best_regression_model():
 
     cwd = os.getcwd()
 
@@ -196,7 +196,77 @@ def find_best_model():
     return best_model, best_hyperparameters, best_metrics
 
 ''' Classification '''
+# Function to turn labels into one hot encoded labels
+def one_hot_encoder(labels, max_Labels = None):
+        """ Takes in label encoded label data and one hot encoded labels. 
 
+        Args:
+            labels (array): the label data.
+            max_labels (int): the number of classes.
+
+        Returns:
+            ohe labels: the one hot encoded labels.
+        """
+        if max_Labels == None:
+            max_Labels = np.max(labels) + 1
+        return np.eye(max_Labels)[labels]
+
+# Return important classification metrics for a model
+def classification_metrics(model, X_train, X_test, y_train, y_test, multiclass = False, multiclass_average = 'macro'):
+
+    y_train_prediction = model.predict(X_train)
+    y_test_prediction = model.predict(X_test)
+
+    if multiclass == False:
+        average = 'binary'
+    else:
+        average = multiclass_average
+
+    f1_score_train = metrics.f1_score(y_train, y_train_prediction, average=average)
+    precision_train = metrics.precision_score(y_train, y_train_prediction, average=average)
+    recall_train = metrics.recall_score(y_train, y_train_prediction, average=average)
+    accuracy_train = metrics.accuracy_score(y_train, y_train_prediction)
+
+    f1_score_test = metrics.f1_score(y_test, y_test_prediction, average=average)
+    precision_test = metrics.precision_score(y_test, y_test_prediction, average=average)
+    recall_test = metrics.recall_score(y_test, y_test_prediction, average=average)
+    accuracy_test = metrics.accuracy_score(y_test, y_test_prediction)
+
+    return {'train': {'f1': f1_score_train,
+                      'precision': precision_train,
+                      'recall': recall_train,
+                      'accuracy': accuracy_train
+                      },
+            'test': {'f1': f1_score_test,
+                     'precision': precision_test,
+                     'recall': recall_test,
+                     'accuracy': accuracy_test}
+            }
+
+# Uses sklearn's GridSearchCV on a classification model to find the best model
+def tune_classification_model_hyperparameters(estimator, X_train, X_test, y_train, y_test, hyperparameters):
+
+    clf = model_selection.GridSearchCV(estimator=estimator,
+                                       scoring='accuracy',
+                                       param_grid=hyperparameters,
+                                       n_jobs=-1)
+
+    clf.fit(X_train, y_train)
+
+    best_model = clf.best_estimator_
+    model_hyperparameters = clf.best_params_
+    validation_accuracy_score = clf.best_score_
+
+    # With ensemble algorithms I will test different estimators but this is not always the case so this if statement handles this
+    if 'estimator' in model_hyperparameters.keys():
+        model_hyperparameters['estimator'] = str(model_hyperparameters['estimator'])
+
+    # Obtain metrics on the train and test data here
+    train_and_test_metrics = classification_metrics(best_model, X_train, X_test, y_train, y_test, multiclass = True)
+
+    best_score_metrics = {'validation_accuracy': validation_accuracy_score, 'train_and_test': train_and_test_metrics}
+
+    return best_model, model_hyperparameters, best_score_metrics
 
 if __name__ == "__main__":
 
@@ -205,9 +275,11 @@ if __name__ == "__main__":
     X = np.array(X)
     y = np.array(y).reshape(-1)
 
-    print(X[:5,:])
-    print()
-    print(y[:5])
+    # Label encode the Category labels
+    le = preprocessing.LabelEncoder()
+    le.fit(y)
+    y = le.transform(y)
+
     # Split data into train, validation and test data
     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
@@ -216,4 +288,16 @@ if __name__ == "__main__":
     X_train = scaler.transform(X_train)
     X_test = scaler.transform(X_test)
 
+    model = linear_model.LogisticRegression(multi_class='multinomial')
 
+    param_grid = {'penalty': ['l1', 'l2', 'elasticnet', None],
+                  'C': [0.25, 0.5, 1, 2],
+                  'max_iter': [50, 100, 500, 1000, 5000]}
+
+    best_model, model_hyperparameters, best_score_metrics = tune_classification_model_hyperparameters(model, X_train, X_test, y_train, y_test, param_grid)
+
+    print(best_model)
+    print()
+    print(model_hyperparameters)
+    print()
+    print(best_score_metrics)
